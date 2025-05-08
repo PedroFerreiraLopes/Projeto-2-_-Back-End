0661ea71515337d1e445d012760ec6770e91e0ca
Antonio Sereno <antoniojsereno@gmail.com>
2025-05-08 14:36:25 -0300

    Dependências e Nomenclaturas
    .

---
+++ src/main/java/br/com/projeto2/aajjl/AajjlApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto2.aajjl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AajjlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AajjlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto2/aajjl/AajjlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto2.aajjl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AajjlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
