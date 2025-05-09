832f8470ae23b1dab3e8ab87a53c50ca8296d02c
Antonio Sereno <antoniojsereno@gmail.com>
2025-05-09 12:32:53 -0300

    Update UserRepository.java

--- src/main/java/br/com/projeto2/aajjl/repository/UserRepository.java
+++ src/main/java/br/com/projeto2/aajjl/repository/UserRepository.java
@@ -4,7 +4,7 @@ import br.com.projeto2.aajjl.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//Aparentemente a anotação @Repository nao é mais necessaria, mas vo deixar pq eu aprendi assim e tbm pra deixar tudo padronizado no Maeven
+//Aparentemente a anotação @Repository nao é mais necessaria, mas vo deixar pq eu aprendi assim e tbm pra deixar tudo padronizado no **Spring**(Maeven so organiza as dependecias do spring e é um fframework tbm )
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
