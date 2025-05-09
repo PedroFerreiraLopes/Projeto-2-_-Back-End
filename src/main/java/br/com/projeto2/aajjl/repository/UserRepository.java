c3651cce5f857dd1114bb461017026513f4be451
Antonio Sereno <antoniojsereno@gmail.com>
2025-05-09 20:58:22 -0300

    User Services

--- src/main/java/br/com/projeto2/aajjl/repository/UserRepository.java
+++ src/main/java/br/com/projeto2/aajjl/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package br.com.projeto2.aajjl.repository;
 
+import br.com.projeto2.aajjl.model.Profissao;
 import br.com.projeto2.aajjl.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//Aparentemente a anotação @Repository nao é mais necessaria, mas vo deixar pq eu aprendi assim e tbm pra deixar tudo padronizado no **Spring**(Maeven so organiza as dependecias do spring e é um fframework tbm )
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByNome(String nome);
+    List<User> findByProfissao(Profissao profissao);
 }
